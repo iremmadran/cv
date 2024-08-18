eac936fc7fbac0269a34135d8917a324889b71d8
İrem Madran <iremonal94@gmail.com>
2024-08-18 20:37:42 +0100

    cv

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {FaStar} from "react-icons/fa6";
 
 function App() {
     return (
@@ -18,7 +19,7 @@ function App() {
                     <div className="port">
                         portfolio
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.5}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
                         preparing
                         <br/>
                         iremmadran.github.io/cv
@@ -26,7 +27,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         contacts
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.5}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
                         irem@madran.co
                         <br/>
                         07477 226 223
@@ -34,7 +35,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         about me
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{paddingRight: 30, lineHeight: 1.5}}>
+                    <div className="ubuntu_regular_secondary" style={{paddingRight: 30, lineHeight: 1.4}}>
                         I have been working as a creative, detailed and responsible interior designer
                         for the last 7 years. With the advancement of technology, I decided to take a
                         new career path in UI/UX design. I believe my experience will support me every
@@ -43,7 +44,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         tools
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.5}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
                         Adobe Illustrator
                         <br/>
                         Adobe Photoshop
@@ -55,45 +56,41 @@ function App() {
                         React
                         <br/>
                         Figma
+                        <br/>
+                        Microsoft Office
                     </div>
                     <div className="ubuntu_medium_main">
                         skills
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.5}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
                         Web Design
-                        <br/>
-                        Visual Design
-                        <br/>
-                        Prototyping + UX/UI
-                        <br/>
-                        3D Design
-                        <br/>
-                        Digital Art
-                        <br/>
-                        Presentation
-                        <br/>
-                        Marketing Research
-                        <br/>
-                        Turkish
-                        <br/>
-                        English
+                        <br/> Visual / 3D Design
+                        <br/> User Experience (UX)
+                        <br/> Usability Testing
+                        <br/> Prototyping & Wireframing
+                        <br/> Digital Art & Drawings
+                        <br/> Presentation
+                        <br/> Marketing Research
+                        <br/> Turkish
+                        <br/> English
                     </div>
                 </div>
                 <div className="right_kol" style={{paddingRight: 30}}>
                     <div className="main_title" style={{marginTop: 30}}>
                         UX/UI Experience
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
                         ux/ui designer, intern
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
-                        Focusbox, UK (2022-2024)
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Focusbox, UK (2022 - 2024)
                     </div>
                     <div className="ubuntu_regular_secondary">
-                       <div style={{lineHeight: 1.5}}>
-                        Designed and prototyped a Pomodoro app for Android and IOS.
+                       <div style={{lineHeight: 1.3}}>
+                           <FaStar style={{fontSize:9}}/> Created an interactive Pomodoro app for Android and iOS that
+                           streamlined user experience through an intuitive interface.
                        </div>
-                        <ul style={{marginTop: 5, columns: 2}}>
+                        <ul style={{marginTop: 2, columns: 3}}>
                             <li>Figma</li>
                             <li style={{marginTop: 3}}>HTML</li>
                             <li style={{marginTop: 3}}>CSS</li>
@@ -103,65 +100,75 @@ function App() {
                     <div className="main_title">
                         Design Experience
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
-                        senior interior architect, freelance
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
+                        interior architect, freelance
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
-                        LaTr Interior, Netherlands (2019-2024)
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        LaTr Interior, Netherlands (2019 - 2024)
                     </div>
                     <div className="ubuntu_regular_secondary">
-                        <div style={{lineHeight: 1.5}}>
-                            Designed customer experience, bespoke furniture.
-                            Created 2D drawings, 3D visuals, realistic renders, mood-boards, presentation sheets.
+                        <div style={{lineHeight: 1.3}}>
+                            <div><FaStar style={{fontSize: 9}}/> Implemented an innovative design process for bespoke
+                            furniture.</div>
+                            <div><FaStar style={{fontSize: 9}}/> Produced technical drawings, 3D visualization & renders,
+                            mood-boards, presentation sheets for 16 different types of projects.</div>
                         </div>
-                        <ul style={{marginTop: 5, columns: 2}}>
+                        <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
                             <li style={{marginTop: 3}}>Archicad</li>
                             <li style={{marginTop: 3}}>Sketchup</li>
                             <li style={{marginTop: 3}}>Lumion</li>
                             <li style={{marginTop: 3}}>Adobe Photoshop</li>
                             <li style={{marginTop: 3}}>Adobe Illustrator</li>
+                            <li style={{marginTop: 3}}>Microsoft Office</li>
                         </ul>
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
-                        senior interior architect
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        interior architect
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
-                        Parşömen Art, Design and Architecture, Turkey (2019-2020)
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Parşömen Art, Design and Architecture, Turkey (2019 - 2020)
                     </div>
                     <div className="ubuntu_regular_secondary">
-                        <div style={{lineHeight: 1.5}}>
-                        Managed engineers and architect teams more than 30 projects. Played an active role in customer
-                        meetings, municipality construction approval, construction and monitoring, and closeout.
-                        Mentored
-                        2 interns. Designed customer experience, bespoke furniture. Created 2D drawings, 3D visuals,
-                        realistic renders, mood-boards, presentation sheets.
+                        <div style={{lineHeight: 1.3, listStyleType:"circle"}}>
+                            <div><FaStar style={{fontSize:9}}/> Directed engineers and architect teams on more than 30
+                            projects.</div>
+                            <div><FaStar style={{fontSize:9}}/> Acted an active role in customer meetings, municipality
+                            construction approval, construction and monitoring, and closeout.</div>
+                            <div><FaStar style={{fontSize:9}}/> Mentored 2 intern architects.</div>
+                            <div><FaStar style={{fontSize:9}}/> Arranged customer experience and shaped bespoke
+                            furniture and interior designs according to customer's reviews.</div>
+                            <div><FaStar style={{fontSize:9}}/> Created technical drawings, modelling and rendering, and
+                            interview documents.</div>
                         </div>
-                        <ul style={{marginTop: 5, columns: 2}}>
+                        <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
                             <li style={{marginTop: 3}}>Archicad</li>
                             <li style={{marginTop: 3}}>Sketchup</li>
                             <li style={{marginTop: 3}}>Lumion</li>
                             <li style={{marginTop: 3}}>Adobe Photoshop</li>
+                            <li style={{marginTop: 3}}>Adobe Illustrator</li>
                             <li style={{marginTop: 3}}>Microsoft Office</li>
                         </ul>
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
                         interior architect
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
-                        MBB Architecture, Turkey (2017-2018)
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        MBB Architecture, Turkey (2017 - 2018)
                     </div>
                     <div className="ubuntu_regular_secondary" >
-                        <div style={{lineHeight: 1.5}}>
-                        Played an active role in customer meetings, municipality construction approval, construction
-                        and monitoring, and closeout. Managed engineers and architect teams more than 50 projects.
-                        Designed
-                        customer experience, bespoke furniture, all interior and exterior designs, all submission
-                        customer experience, bespoke furniture, all interior and exterior designs, all submission
-                        drawings. Created 2D & 3D designs, realistic renders, mood-boards, presentation sheets.
+                        <div style={{lineHeight: 1.3}}>
+                        <div><FaStar style={{fontSize:9}}/> Played an active role in customer meetings, municipality
+                        construction approval, construction and monitoring, and closeout.</div>
+                        <div><FaStar style={{fontSize:9}}/> Coordinated with engineers and architect teams for more than
+                        50 projects.</div>
+                        <div><FaStar style={{fontSize:9}}/> Designed customer experience, bespoke furniture, and all
+                        interior and exterior designs suitable for customer budgets.</div>
+                        <div><FaStar style={{fontSize:9}}/> Prepared 2D & 3D visualization, realistic renders, mood
+                        boards, and presentation sheets. </div>
                         </div>
-                        <ul style={{marginTop: 5, columns: 2}}>
+                        <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
                             <li style={{marginTop: 3}}>Archicad</li>
                             <li style={{marginTop: 3}}>Adobe Photoshop</li>
@@ -171,41 +178,41 @@ function App() {
                     <div className="main_title">
                         Exhibitions
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
                         university exhibitions
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
-                        Turkey (2016 and 2015)
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Turkey (2015 - 2016)
                     </div>
                     <div className="ubuntu_regular_secondary">
-                        <ul style={{marginTop: 5}}>
+                        <ul style={{marginTop: 2}}>
                             <li>Restoration - Traditional Trabzon Handcraft Workshop</li>
                             <li style={{marginTop: 3}}>Modern Furniture - ‘Scissors Hands’ Surface and Furniture Design
                             </li>
                             <li style={{marginTop: 3}}>Modern Furniture - ‘Scissors Hands’ Poster Design</li>
                             <li style={{marginTop: 3}}>Feza van der Weele House & Office Design</li>
-                            <li style={{marginTop: 3}}>Sitting Element - from song to furniture ‘Mystic’</li>
+                            <li style={{marginTop: 3}}>Seating Element- ‘Mystic’ from song to furniture</li>
                             <li style={{marginTop: 3}}>Orange County Choppers (OCC) Showroom</li>
                         </ul>
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
                         1st national interior design symposium
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
                         Turkey (Oct 2015)
                     </div>
                     <div className="ubuntu_regular_secondary">
-                        <ul style={{marginTop: 5}}>
+                        <ul style={{marginTop: 2}}>
                             <li>Feza van der Weele House & Office Design</li>
                         </ul>
                     </div>
                     <div className="main_title">
                         Education
                     </div>
-                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 5}}>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
                         karadeniz technical university
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray"}}>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
                         Turkey (2012-2017)
                     </div>
                     <div className="ubuntu_regular_secondary" style={{marginBottom: 30}}>
