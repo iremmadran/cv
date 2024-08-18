dc52a8de68e68db426a22a500ba9f17192ee7d29
İrem Madran <iremonal94@gmail.com>
2024-08-18 21:28:36 +0100

    cv

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ function App() {
                     <div className="job_title">
                         UI/UX Designer
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{marginTop: 15}}>
+                    <div className="ubuntu_regular_secondary">
                         London, UK
                     </div>
                     <div className="port">
                         portfolio
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
                         preparing
                         <br/>
                         iremmadran.github.io/cv
@@ -27,7 +27,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         contacts
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
                         irem@madran.co
                         <br/>
                         07477 226 223
@@ -35,7 +35,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         about me
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{paddingRight: 30, lineHeight: 1.4}}>
+                    <div className="ubuntu_regular_secondary" style={{paddingRight: 25, lineHeight: 1.3}}>
                         I have been working as a creative, detailed and responsible interior designer
                         for the last 7 years. With the advancement of technology, I decided to take a
                         new career path in UI/UX design. I believe my experience will support me every
@@ -44,7 +44,7 @@ function App() {
                     <div className="ubuntu_medium_main">
                         tools
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
                         Adobe Illustrator
                         <br/>
                         Adobe Photoshop
@@ -62,21 +62,22 @@ function App() {
                     <div className="ubuntu_medium_main">
                         skills
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.4}}>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
                         Web Design
-                        <br/> Visual / 3D Design
+                        <br/> Visual Design
                         <br/> User Experience (UX)
+                        <br/> Responsive & Accessibility Design
                         <br/> Usability Testing
                         <br/> Prototyping & Wireframing
                         <br/> Digital Art & Drawings
                         <br/> Presentation
-                        <br/> Marketing Research
+                        <br/> Marketing & User Research
                         <br/> Turkish
                         <br/> English
                     </div>
                 </div>
                 <div className="right_kol" style={{paddingRight: 30}}>
-                    <div className="main_title" style={{marginTop: 30}}>
+                    <div className="main_title">
                         UX/UI Experience
                     </div>
                     <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
@@ -87,8 +88,8 @@ function App() {
                     </div>
                     <div className="ubuntu_regular_secondary">
                        <div style={{lineHeight: 1.3}}>
-                           <FaStar style={{fontSize:9}}/> Created an interactive Pomodoro app for Android and iOS that
-                           streamlined user experience through an intuitive interface.
+                           <FaStar style={{fontSize:9}}/> Created and developed a user-centric Pomodoro app for Android
+                           and iOS, enhancing productivity through an intuitive interface and streamlined user experience.
                        </div>
                         <ul style={{marginTop: 2, columns: 3}}>
                             <li>Figma</li>
@@ -108,10 +109,14 @@ function App() {
                     </div>
                     <div className="ubuntu_regular_secondary">
                         <div style={{lineHeight: 1.3}}>
-                            <div><FaStar style={{fontSize: 9}}/> Implemented an innovative design process for bespoke
-                            furniture.</div>
-                            <div><FaStar style={{fontSize: 9}}/> Produced technical drawings, 3D visualization & renders,
-                            mood-boards, presentation sheets for 16 different types of projects.</div>
+                            <div><FaStar style={{fontSize: 9}}/> Implemented an innovative design process for bespoke furniture.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Crafted technical drawings, 3D visualization & renders,
+                                mood-boards for 16 diverse projects, boosting project approval rates.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Produced detailed presentation sheets, facilitating clear
+                                client communication and project alignment.
+                            </div>
                         </div>
                         <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
@@ -131,15 +136,16 @@ function App() {
                     </div>
                     <div className="ubuntu_regular_secondary">
                         <div style={{lineHeight: 1.3, listStyleType:"circle"}}>
-                            <div><FaStar style={{fontSize:9}}/> Directed engineers and architect teams on more than 30
-                            projects.</div>
-                            <div><FaStar style={{fontSize:9}}/> Acted an active role in customer meetings, municipality
-                            construction approval, construction and monitoring, and closeout.</div>
-                            <div><FaStar style={{fontSize:9}}/> Mentored 2 intern architects.</div>
+                            <div><FaStar style={{fontSize:9}}/> Led teams on 30+ projects, ensuring timely completion
+                            and adherence to design standards.</div>
+                            <div><FaStar style={{fontSize:9}}/> Facilitated client meetings and secured municipal
+                            construction approval, construction and monitoring, enhancing project success.</div>
+                            <div><FaStar style={{fontSize:9}}/> Guided 2 intern architects, fostering professional growth
+                            and skill development.</div>
                             <div><FaStar style={{fontSize:9}}/> Arranged customer experience and shaped bespoke
-                            furniture and interior designs according to customer's reviews.</div>
+                            furniture and interiors based on client feedback, improving customer satisfaction. </div>
                             <div><FaStar style={{fontSize:9}}/> Created technical drawings, modelling and rendering, and
-                            interview documents.</div>
+                            documentation, supporting project execution.</div>
                         </div>
                         <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
@@ -159,14 +165,14 @@ function App() {
                     </div>
                     <div className="ubuntu_regular_secondary" >
                         <div style={{lineHeight: 1.3}}>
-                        <div><FaStar style={{fontSize:9}}/> Played an active role in customer meetings, municipality
-                        construction approval, construction and monitoring, and closeout.</div>
-                        <div><FaStar style={{fontSize:9}}/> Coordinated with engineers and architect teams for more than
-                        50 projects.</div>
-                        <div><FaStar style={{fontSize:9}}/> Designed customer experience, bespoke furniture, and all
-                        interior and exterior designs suitable for customer budgets.</div>
+                        <div><FaStar style={{fontSize:9}}/> Led customer meetings, municipality construction approval,
+                        and managed project closeouts for 50+ projects.</div>
+                        <div><FaStar style={{fontSize:9}}/> Collaborated with engineers and architects, ensuring seamless
+                        coordination.</div>
+                        <div><FaStar style={{fontSize:9}}/> Designed customer experience, bespoke furniture, and
+                        interior / exterior layouts within budget, enhancing customer satisfaction.</div>
                         <div><FaStar style={{fontSize:9}}/> Prepared 2D & 3D visualization, realistic renders, mood
-                        boards, and presentation sheets. </div>
+                        boards, and presentation sheets for clients. </div>
                         </div>
                         <ul style={{marginTop: 2, columns: 3}}>
                             <li>AutoCAD</li>
@@ -215,7 +221,7 @@ function App() {
                     <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
                         Turkey (2012-2017)
                     </div>
-                    <div className="ubuntu_regular_secondary" style={{marginBottom: 30}}>
+                    <div className="ubuntu_regular_secondary">
                         Bachelor of Interior Architect
                     </div>
                 </div>
