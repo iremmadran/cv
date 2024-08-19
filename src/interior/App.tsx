49c28b6e761cb086b8e98f4d2b47be5284eb7bb4
İrem Madran <iremonal94@gmail.com>
2024-08-19 09:12:54 +0100

    interior

---
+++ src/interior/App.tsx
@@ -0,0 +1,240 @@
+import React from 'react';
+import './App.css';
+import {FaStar} from "react-icons/fa6";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="a4">
+                <div className="left_kol">
+                    <div className="irem_madran">
+                        İrem Madran
+                    </div>
+                    <div className="job_title">
+                        Interior Architect
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        London, UK
+                    </div>
+                    <div className="ubuntu_medium_main">
+                        contacts
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
+                        irem@madran.co
+                        <br/>
+                        07477 226 223
+                    </div>
+                    <div className="ubuntu_medium_main">
+                        about me
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{paddingRight: 25, lineHeight: 1.3}}>
+                        at Pixxi Handmade, we crafted a brand blending hobby with professional craftsmanship,
+                        specializing in miniatures and role-play game accessories. My background in architecture
+                        informs my design approach, combining creativity with structure. As an Interior Architect,
+                        I deliver bespoke residential solutions that merge technical skill with aesthetic finesse.
+                    </div>
+                    <div className="ubuntu_medium_main">
+                        tools
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
+                        Autocad
+                        <br/>Sketchup
+                        <br/>Adobe Illustrator
+                        <br/>Adobe Photoshop
+                        <br/>Archicad
+                        <br/>Lumion
+                        <br/>Microsoft Office
+                    </div>
+                    <div className="ubuntu_medium_main">
+                        skills
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{lineHeight: 1.3}}>
+                        Interior Design
+                        <br/> Visual Design
+                        <br/> Technical Drawings
+                        <br/> Responsive & Accessibility Design
+                        <br/> Digital Art & Drawings
+                        <br/> Presentation
+                        <br/> Researching  Detailer
+                        <br/> Multi-Tasking
+                        <br/> Turkish
+                        <br/> English
+                    </div>
+                </div>
+                <div className="right_kol" style={{paddingRight: 30}}>
+                    <div className="main_title">
+                        Professional Experience
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
+                        interior architect, freelance
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        LaTr Interior, Netherlands (2019 - 2024)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <div style={{lineHeight: 1.3}}>
+                            <div><FaStar style={{fontSize: 9}}/> Implemented an innovative design process for bespoke
+                                furniture.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Crafted technical drawings, 3D visualization & renders,
+                                mood-boards for 16 diverse projects, boosting project approval rates.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Produced detailed presentation sheets, facilitating
+                                clear
+                                client communication and project alignment.
+                            </div>
+                        </div>
+                        <ul style={{marginTop: 2, columns: 3}}>
+                            <li>AutoCAD</li>
+                            <li style={{marginTop: 3}}>Archicad</li>
+                            <li style={{marginTop: 3}}>Sketchup</li>
+                            <li style={{marginTop: 3}}>Lumion</li>
+                            <li style={{marginTop: 3}}>Adobe Photoshop</li>
+                            <li style={{marginTop: 3}}>Adobe Illustrator</li>
+                            <li style={{marginTop: 3}}>Microsoft Office</li>
+                        </ul>
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        interior architect
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Parşömen Art, Design and Architecture, Turkey (2019 - 2020)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <div style={{lineHeight: 1.3, listStyleType: "circle"}}>
+                            <div><FaStar style={{fontSize: 9}}/> Led teams on 30+ projects, ensuring timely completion
+                                and adherence to design standards.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Facilitated client meetings and secured municipal
+                                construction approval, construction and monitoring, enhancing project success.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Guided 2 intern architects, fostering professional
+                                growth
+                                and skill development.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Arranged customer experience and shaped bespoke
+                                furniture and interiors based on client feedback, improving customer satisfaction.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Created technical drawings, modelling and rendering,
+                                and
+                                documentation, supporting project execution.
+                            </div>
+                        </div>
+                        <ul style={{marginTop: 2, columns: 3}}>
+                            <li>AutoCAD</li>
+                            <li style={{marginTop: 3}}>Archicad</li>
+                            <li style={{marginTop: 3}}>Sketchup</li>
+                            <li style={{marginTop: 3}}>Lumion</li>
+                            <li style={{marginTop: 3}}>Adobe Photoshop</li>
+                            <li style={{marginTop: 3}}>Adobe Illustrator</li>
+                            <li style={{marginTop: 3}}>Microsoft Office</li>
+                        </ul>
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        interior architect
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        MBB Architecture, Turkey (2017 - 2018)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <div style={{lineHeight: 1.3}}>
+                            <div><FaStar style={{fontSize: 9}}/> Led customer meetings, municipality construction
+                                approval,
+                                and managed project closeouts for 50+ projects.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Collaborated with engineers and architects, ensuring
+                                seamless
+                                coordination.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Designed customer experience, bespoke furniture, and
+                                interior / exterior layouts within budget, enhancing customer satisfaction.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Prepared 2D & 3D visualization, realistic renders, mood
+                                boards, and presentation sheets for clients.
+                            </div>
+                        </div>
+                        <ul style={{marginTop: 2, columns: 3}}>
+                            <li>AutoCAD</li>
+                            <li style={{marginTop: 3}}>Archicad</li>
+                            <li style={{marginTop: 3}}>Adobe Photoshop</li>
+                            <li style={{marginTop: 3}}>Microsoft Office</li>
+                        </ul>
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        intern interior architect
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Ceylanlar Construction, Turkey (2015)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <div style={{lineHeight: 1.3}}>
+                            <div><FaStar style={{fontSize: 9}}/> Collaborated with an interior architect on boutique
+                                hotel design projects, creating 2D floor plans and sections.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Developed 3D visualizations and realistic renders to
+                                enhance project presentations.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Drafted bespoke furniture designs, ensuring alignment
+                                with project aesthetics.
+                            </div>
+                            <div><FaStar style={{fontSize: 9}}/> Prepared fire and emergency exit plans, adhering to
+                                safety regulations.
+                            </div>
+                        </div>
+                        <ul style={{marginTop: 2, columns: 3}}>
+                            <li>AutoCAD</li>
+                            <li style={{marginTop: 3}}>Archicad</li>
+                            <li style={{marginTop: 3}}>Adobe Photoshop</li>
+                            <li style={{marginTop: 3}}>Microsoft Office</li>
+                        </ul>
+                    </div>
+                    <div className="main_title">
+                        Exhibitions
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        university exhibitions
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Turkey (2015 - 2016)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <ul style={{marginTop: 2}}>
+                            <li>Restoration - Traditional Trabzon Handcraft Workshop</li>
+                            <li style={{marginTop: 3}}>Modern Furniture - ‘Scissors Hands’ Surface and Furniture Design
+                            </li>
+                            <li style={{marginTop: 3}}>Modern Furniture - ‘Scissors Hands’ Poster Design</li>
+                            <li style={{marginTop: 3}}>Feza van der Weele House & Office Design</li>
+                            <li style={{marginTop: 3}}>Seating Element - ‘Mystic’ from song to furniture</li>
+                            <li style={{marginTop: 3}}>Orange County Choppers (OCC) Showroom</li>
+                        </ul>
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 1}}>
+                        1st national interior design symposium
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Turkey (Oct 2015)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        <ul style={{marginTop: 2}}>
+                            <li>Feza van der Weele House & Office Design</li>
+                        </ul>
+                    </div>
+                    <div className="main_title">
+                        Education
+                    </div>
+                    <div className="ubuntu_medium_main" style={{fontSize: 16, marginTop: 4}}>
+                        karadeniz technical university
+                    </div>
+                    <div className="ubuntu_regular_secondary" style={{fontSize: 10, color: "darkgray", marginTop: 2}}>
+                        Turkey (2012-2017)
+                    </div>
+                    <div className="ubuntu_regular_secondary">
+                        Bachelor of Interior Architect
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
